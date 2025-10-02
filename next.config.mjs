def418e8aed99e1788fac197bc92bcd57ab26100
Agrannya-Singh <singh.agrannya@gmail.com>
2025-10-02 22:15:47 +0530

    fix directory issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
-/** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
   outputFileTracingRoot: __dirname,
